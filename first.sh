9034c4bc1cfa9062ee0d74f39e1eca0bb0c6bd41
mukul <mukulshivam90@gmail.com>
2021-06-25 14:25:25 +0530

    press 1 script

---
+++ first.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+
+
+clear 
+
+echo "Press 1 for create the directory"  	
+select data in 2 
+do 
+	if [ "$data" == 1 ];
+	then
+
+	echo "Enter directory name"
+	read dirname
+
+	if [ ! -d "$dirname" ]
+	then
+    		echo "Directory doesn't exist. Creating now"
+    		mkdir ./$dirname
+    		echo "Directory created"
+	else
+   		 echo "Directory exists"
+		 
+	fi
+	fi 
+	
+done
+ 
